e7fb41b40f973bfb58121131c445e73043b187df
wtshek <wtshek119@gmail.com>
2025-06-10 05:45:47 -0400

    add cloudflare

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,13 +6,13 @@ import sitemap from '@astrojs/sitemap';
 import tailwind from '@astrojs/tailwind';
 import { defineConfig } from 'astro/config';
 
+import cloudflare from '@astrojs/cloudflare';
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://wingtungshek.com/',
   output: 'server',
-  adapter: node({
-    mode: 'standalone',
-  }),
+  adapter: cloudflare(),
   integrations: [
     mdx(),
     sitemap(),
