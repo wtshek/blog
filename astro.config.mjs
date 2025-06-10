5d9508b7a2e20b7c69726eb53fad6bf5f5237357
wtshek <wtshek119@gmail.com>
2025-06-10 05:56:58 -0400

    remove react import

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,5 @@
 // @ts-check
 import mdx from '@astrojs/mdx';
-import react from '@astrojs/react';
 import sitemap from '@astrojs/sitemap';
 import tailwind from '@astrojs/tailwind';
 import { defineConfig } from 'astro/config';
@@ -15,7 +14,6 @@ export default defineConfig({
   integrations: [
     mdx(),
     sitemap(),
-    react(),
     tailwind({
       applyBaseStyles: false, // We're using our custom global.css
     }),
